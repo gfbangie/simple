a2fc638ffa110158239f765989bb585976d1848d
gfbangie <goolam.fareed123@gmail.com>
2020-03-12 19:03:33 +0200

    Added file

---
+++ login.js
@@ -0,0 +1,15 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCWens99pTDuzFOVe5V5P9gi_8k-Vmj6-U",
+    authDomain: "html-login-a5565.firebaseapp.com",
+    databaseURL: "https://html-login-a5565.firebaseio.com",
+    projectId: "html-login-a5565",
+    storageBucket: "html-login-a5565.appspot.com",
+    messagingSenderId: "570852111328",
+    appId: "1:570852111328:web:4c4797d562946a0f093185"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+ 
+ const auth =firebase.auth();
