7f07613557644098a619b02f62935d820a167abf
gfbangie <61416962+gfbangie@users.noreply.github.com>
2020-03-15 17:22:55 +0200

    Update ProjectJavaApplicationTests.java

--- src/test/java/com/example/projectJava/ProjectJavaApplicationTests.java
+++ src/test/java/com/example/projectJava/ProjectJavaApplicationTests.java
@@ -2,12 +2,17 @@ package com.example.projectJava;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.*;
+import static org.junit.Assert.*;
+import org.junit.*;
 
 @SpringBootTest
-class ProjectJavaApplicationTests {
+public class ProjectJavaApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testAdd() {
+		ProjectJavaApplication p=new ProjectJavaApplication();
+		assertEquals(p.add(1,1),2);
 	}
 
 }
